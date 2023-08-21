c486734ba7b156bbec3cd61e061eb9c885f77d75
shiva khatri <shiva.khat23@yahoo.com>
2023-08-21 18:22:24 +0200

    add timer to run simulation every 1 sec

--- src/App.tsx
+++ src/App.tsx
@@ -12,11 +12,12 @@ import { Point } from './Utils/Point.ts';
 function App() {
   let [isStarted, setIsStarted] = useState(false);
   const canvasRef = useRef<ElementRef<'canvas'>>(null);
+  const squareSizeRef = useRef(20);
+  const numberOfColumnsRef = useRef(60);
+  const numberOfRowsRef = useRef(30);
+  const cellsRef = useRef<Cell[]>([]);
 
-  const squareSize = 20;
-  const numberOfColumns = 60;
-  const numberOfRows = 30;
-  let cells: Cell[] = [];
+  let timerRef = useRef(null);
 
   useEffect(() => {
     console.log('canvas rendered !');
@@ -24,11 +25,15 @@ function App() {
       const canvas: HTMLCanvasElement = canvasRef.current;
       const context: CanvasRenderingContext2D = canvas.getContext('2d') as CanvasRenderingContext2D;
 
+      const numberOfColumns = numberOfColumnsRef.current;
+      const numberOfRows = numberOfRowsRef.current;
+      const squareSize = squareSizeRef.current;
 
       canvas.width = squareSize * numberOfColumns;
       canvas.height = squareSize * numberOfRows;
 
-      cells = [];
+      const cells: Cell[] = cellsRef.current;
+      cells.length = 0;
 
       for (let i = 0; i < numberOfRows; i++) {
         for (let j = 0; j < numberOfColumns; j++) {
@@ -42,37 +47,40 @@ function App() {
     }
   }, []);
 
+  useEffect(() => {
+    if (isStarted) {
+      timerRef.current = setInterval(() => {
+        console.log(`timer called !: ${isStarted}`);
+        startGameOfLife(cellsRef.current);
+      }, 1000);
+      return () => clearInterval(timerRef.current);
+    } else {
+      clearInterval(timerRef.current);
+    }
+  }, [isStarted]);
+
   function highlightGridItemAndNeibour(e: React.MouseEvent<HTMLCanvasElement, MouseEvent>) {
     console.log(e);
     const canvas: HTMLCanvasElement = canvasRef.current as HTMLCanvasElement;
     const canvasRect = canvas.getBoundingClientRect();
-    let index_x = e.clientX - canvasRect.left;
-    let index_y = e.clientY - canvasRect.top;
 
-    let i = Math.floor(index_x / squareSize);
-    let j = Math.floor(index_y / squareSize);
+    const numberOfColumns = numberOfColumnsRef.current;
+    const squareSize = squareSizeRef.current;
+
+    let columnX = e.clientX - canvasRect.left;
+    let columnY = e.clientY - canvasRect.top;
+
+    let i = Math.floor(columnX / squareSize);
+    let j = Math.floor(columnY / squareSize);
 
     let index = j * numberOfColumns + i;
 
-    cells[index].toggle();
-
-    for (let m = -1; m <= 1; m++) {
-      for (let n = -1; n <= 1; n++) {
-        if (!(m == 0 && n == 0)) {
-          let neibour_x = i + m;
-          let neibour_y = j + n;
-          if (neibour_x > -1 && neibour_y > -1 && neibour_x < numberOfColumns && neibour_y < numberOfRows) {
-            let neibour_index = neibour_y * numberOfColumns + neibour_x;
-            if (neibour_index > 0 && neibour_index < cells.length) {
-              cells[neibour_index].toggle();
-            }
-          }
-        }
-      }
-    }
+    cellsRef.current[index].toggle();
   }
 
-  function startGameOfLife() {
+  function startGameOfLife(cells: Cell[]) {
+    const numberOfColumns = numberOfColumnsRef.current;
+    const numberOfRows = numberOfRowsRef.current;
     let cellsToAliveNeibours: Map<number, number> = new Map<number, number>();
     cells.forEach((cell, index) => {
       let numberOfAliveNeibours = 0;
@@ -101,7 +109,6 @@ function App() {
 
   }
 
-
   return (
     <>
       <CssBaseline />
@@ -117,7 +124,7 @@ function App() {
             variant='contained'
             startIcon={isStarted ? <PauseIcon /> : <PlayArrowIcon />}
             sx={{ m: 1 }}
-            onClick={startGameOfLife}
+            onClick={() => setIsStarted(!isStarted)}
           >
             Start
           </Button>
